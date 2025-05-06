88fdeb4fa98d099c39777a2c0f15da96ef5ebd48
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2025-05-06 15:29:29 +0300

    add order service with placeorder method

---
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.example.order_service.service;
+
+import com.example.order_service.dto.OrderLineItemsDto;
+import com.example.order_service.dto.OrderRequest;
+import com.example.order_service.model.Order;
+import com.example.order_service.model.OrderLineItems;
+import com.example.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsList()
+                .stream()
+                .map(this::mapToOrderLineItem)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToOrderLineItem(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        return orderLineItems;
+    }
+}
