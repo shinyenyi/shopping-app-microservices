caf816ffd5aa211194e193d4b9092663fae19473
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2025-05-06 15:11:31 +0300

    add orderrequest and orderlineitems dtos

---
+++ order-service/src/main/java/com/example/order_service/dto/OrderLineItemsDto.java
@@ -0,0 +1,17 @@
+package com.example.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
